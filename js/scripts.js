0052ef99e63dd5a442e219e640dfc4bb8f8603bc
abby <abrown13@eagles.ewu.edu>
2018-10-30 11:39:46 -0700

    Successfully got weather

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-$('li').text('OK');
+$.simpleWeather({
+    location: 99004,
+    success: function(weather) {
+        console.log(weather.image);
+        console.log(weather.city);
+        console.log(weather.temp);
+        console.log(weather.updated);
+        
+        $('.image img').attr('src', weather.image);
+        $('.city').text(weather.city);
+        $('.temp').text(weather.temp);
+        $('.time').text(weather.updated);
+    }, 
+    error: function(error) {
+      console.log('Go outside');
+    }
+ });
