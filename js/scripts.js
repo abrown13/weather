b458f72eceeed3963af876dd6742a3ce8665d6ae
Abby <abrown13@eagles.ewu.edu>
2018-11-24 22:27:11 -0800

    tried to add image classes

--- js/scripts.js
+++ js/scripts.js
@@ -10,22 +10,11 @@ $.simpleWeather({
       $('i').addClass('icon-' + weather.code);
       $('.spangle').text(weather.city);
       $('.spangle-temp').text(weather.temp);
-
-      // Change Image
-      if ( weather.code >= 0 && weather.code <= 12 ) {
-        $('.card-image').addClass('rainy');
-    }
-
-      if ( weather.code >= 13 && weather.code <= 18 ) {
-      $('.card-image').addClass('snowy');
-
+      
       if ( weather.code >= 23 && weather.code <= 30 ) {
         $('.card-image').addClass('windy');
+    }
 
-      if ( weather.code >= 31 && weather.code <= 34 ) {
-        $('.card-image').addClass('sunny');
-
-      
     },
     error: function(error) {
       // Show if weather cannot be retreived
@@ -56,3 +45,23 @@ $.simpleWeather({
   });
 
 // Geolocation
+
+
+
+
+// Change Image
+    //   if ( weather.code >= 0 && weather.code <= 12 ) {
+    //     $('.card-image').addClass('rainy');
+    // }
+
+    //   if ( weather.code >= 13 && weather.code <= 18 ) {
+    //     $('.card-image').addClass('snowy');
+    // }
+
+    //   if ( weather.code >= 23 && weather.code <= 30 ) {
+    //     $('.card-image').addClass('windy');
+    // }
+
+    //   if ( weather.code >= 31 && weather.code <= 34 ) {
+    //     $('.card-image').addClass('sunny');
+    // }
