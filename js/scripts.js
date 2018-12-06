96f7f4f8e1aa93b207964d9c2e8b774a30ace1ba
Abby <abrown13@eagles.ewu.edu>
2018-12-06 12:38:42 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,58 @@
+// City One
+$.simpleWeather({
+  location: 99031,
+  unit: 'f',
+  success: function(weather) {
+    // Entire weather object
+    console.log(weather);
+    
+    // Display Data
+    $('i').addClass('icon-' + weather.code);
+    $('#city1 .city').text(weather.city);
+    $('#city1 .temp').text(weather.temp);
+    $('#city1 .currently').text(weather.currently);
+    $('#city1 .high').text(weather.high);
+    $('#city1 .low').text(weather.low);
+
+    if ( weather.code >= 29 && weather.code <= 36 ) {
+      $('body').addClass('sun-shower');
+  }
+
+  },
+  error: function(error) {
+    // Show if weather cannot be retreived
+    console.log('Look outside.');
+  }
+
+});
+
+// City Two
+$.simpleWeather({
+  location: 'Spokane, WA',
+  unit: 'f',
+  success: function(weather) {
+    // Entire weather object
+    console.log(weather);
+    
+    // Display Data
+    $('i').addClass('icon-' + weather.code);
+    $('#city2 .city').text(weather.city);
+    $('#city2 .temp').text(weather.temp);
+    $('#city2 .currently').text(weather.currently);
+    $('#city2 .high').text(weather.high);
+    $('#city2 .low').text(weather.low);
+    
+  },
+  error: function(error) {
+    // Show if weather cannot be retreived
+    console.log('Look outside.');
+  }
+
+});
+
+
+
+
 // Geolocation
 // Geolocation Check
 if ( 'geolocation' in navigator ) {
@@ -26,6 +81,7 @@ $('button').click( function(){
    getWeather( lat + ',' + long );
     
   });
+
 });
 
 
@@ -58,57 +114,5 @@ var getWeather = function( location ) {
 
 });
 
-
-// City One
-$.simpleWeather({
-    location: 99031,
-    unit: 'f',
-    success: function(weather) {
-      // Entire weather object
-      console.log(weather);
-      
-      // Display Data
-      $('i').addClass('icon-' + weather.code);
-      $('#city1 .city').text(weather.city);
-      $('#city1 .temp').text(weather.temp);
-      $('#city1 .currently').text(weather.currently);
-      $('#city1 .high').text(weather.high);
-      $('#city1 .low').text(weather.low);
-
-      if ( weather.code >= 29 && weather.code <= 36 ) {
-        $('body').addClass('sun-shower');
-    }
-
-    },
-    error: function(error) {
-      // Show if weather cannot be retreived
-      console.log('Look outside.');
-    }
-  
-  });
-
-// City Two
-$.simpleWeather({
-    location: 'Spokane, WA',
-    unit: 'f',
-    success: function(weather) {
-      // Entire weather object
-      console.log(weather);
-      
-      // Display Data
-      $('i').addClass('icon-' + weather.code);
-      $('#city2 .city').text(weather.city);
-      $('#city2 .temp').text(weather.temp);
-      $('#city2 .currently').text(weather.currently);
-      $('#city2 .high').text(weather.high);
-      $('#city2 .low').text(weather.low);
-      
-    },
-    error: function(error) {
-      // Show if weather cannot be retreived
-      console.log('Look outside.');
-    }
-  
-  });
 
 };
